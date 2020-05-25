fac3b402bc73d930fefa5caec5bbd4b83625a576
Iris Huang <lyaui@outlook.com>
2020-05-26 01:48:56 +0800

    first commit

---
+++ src/pages/PageSettings.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-page padding>
+    Lorem consectetur iquam nemo inventore unde aliquid, id ducimus veritatis!
+  </q-page>
+</template>
---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <h5>{{message}}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'I Love Vue.js',
+    };
+  },
+};
+</script>
+<style></style>
